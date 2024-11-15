b1acbdf877c54757f5016e2ad69b4fae65d2c752
matze2006 <matthias.h06@gmail.com>
2024-11-15 16:32:46 +0100

    start js + neue Ordner

---
+++ JavaScript/01 JS Variablen_Operationen/script.js
@@ -0,0 +1,47 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("Variablen");
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+
+// Rechnungsarten
+// *, /, +, -
+
+console.log("Rechnungen");
+
+let result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a + b) / c;
+console.log("(a + b) / c = " + result);
+
+reslut = c - a + b;
+console.log("c -a + b = " + result);
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result);
+
+
+//neue Variablen
+
+console.log("neue Variablen");
+
+a = 3;
+console.log("a = " + a);
+
+b = a + 3
+console.log("b = " + b);
+
+c = c -3
+console.log("c = " + c);
---
+++ JavaScript/02 JS String_Zahlen/script.js
@@ -0,0 +1,36 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c + d;
+console.log(result);
+
+// Test STRINGS
+
+let name = "Matthias";
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+// Aufgaben
+
+console.log("Aufgabe 1");
+
+a = 17.4;
+b = 3.9;
+
+result = a / b;
+console.log("The result of " + a + " divided by " + b + " is " + result);
+
+console.log("Aufgabe 2");
+
+let firstname = "Matthias";
+let lastname = "Hagen";
+
+console.log(firstname + " " + lastname);
