0cf17d2e08277fb829ceef4f9ab4a42b173484f2
matze2006 <matthias.h06@gmail.com>
2024-12-03 09:25:31 +0100

    05 If-Bedingung done

---
+++ JavaScript/05 If-Bedingung/script.js
@@ -0,0 +1,32 @@
+let number = 100;
+
+//Aufagbe 1
+if (number < 20)
+{
+    console.log("Mini");
+}
+
+if (number > 20 && number < 50)
+{
+    console.log("Medium");
+}
+
+if (number > 50)
+{
+    console.log("Large");
+}
+
+//Aufgabe 2
+
+if (number <= 7)
+{
+    console.log("Mini");
+}
+else if(number > 7 && number < 90)
+{
+    console.log("Medium");
+}
+else
+{
+    console.log("Large")
+}
