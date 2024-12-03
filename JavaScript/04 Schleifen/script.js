5203173d9c3c8a32d9eddc7c15d9cf486091e04d
matze2006 <matthias.h06@gmail.com>
2024-12-03 09:10:47 +0100

    04 Schleifen done

---
+++ JavaScript/04 Schleifen/script.js
@@ -0,0 +1,48 @@
+
+//Zähle von 7 bis 45
+for (let count = 7; count <= 45; count++)
+{
+    console.log(count);
+}
+
+//_________________________________________
+//Zähle von 9 bis 99
+for(let count2 = 9; count2 <= 99; count2++)
+{
+    console.log(count2);
+}
+
+//_________________________________________
+//Zähle rückwärts von 10 bis 0
+for(let count3 = 10; count3 >= 0; count3--)
+{
+    console.log(count3);
+}
+
+//_________________________________________
+//Zähle rückwärts von 1234 bis 1207
+for(let count4 = 1234; count4 >= 1207; count4--)
+{
+    console.log(count4);
+}
+
+//_________________________________________
+//Gib Deinen Namen genau 8 mal aus
+for(let count = 0; count < 8; count++)
+{
+    console.log("Matthias");
+}
+
+//_________________________________________
+//Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for(let count5_1 = 0; count5_1 <= 10; count5_1++)
+{
+    console.log(count5_1);
+}
+
+console.log("Matthias")
+
+for(let count5_2 = 10; count5_2 >= 0; count5_2--)
+{
+    console.log(count5_2);
+}
