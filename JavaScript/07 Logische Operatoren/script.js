18c025dd1a8736f255aae8b32a60c75024df0717
matze2006 <matthias.h06@gmail.com>
2024-12-10 09:29:02 +0100

    07 Logische Operatoren done

---
+++ JavaScript/07 Logische Operatoren/script.js
@@ -0,0 +1,34 @@
+// Erstelle zwei Zufallszahl zwischen 0 und 100
+// Speichere diese jeweils in einer Variable
+
+let randomNumber1 = Math.random() * 100;
+let randomNumber2 = Math.random() * 100; 
+let random1 = Math.floor(randomNumber1);
+let random2 = Math.floor(randomNumber2);
+
+console.log(random1);
+console.log(random2);
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+// dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+
+if (random1 < random2 && random1 < 50)
+{
+    console.log("Zahl 1 ist kliener als Zahl 2 und Mini");
+}
+
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+// dann gib aus "Eine der beiden ist kleiner als 30"
+
+if (random1 < 30 || random2 < 30)
+{
+    console.log("Eine der beiden ist kleiner als 30");
+}
+
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+// dann gib aus "Erste Zahl klein, zweite kein 50iger"
+
+if (random1 < 50 && random2 != 50)
+{
+    console.log("Erste Zahl klein, zweite kein 50iger");
+}
