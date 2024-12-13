ac0966cb82dee170fb1b4b57bcfa5c7e4267e676
matze2006 <matthias.h06@gmail.com>
2024-12-13 16:07:22 +0100

    08 Switch Statement done

---
+++ JavaScript/08 Switch Statement/script.js
@@ -0,0 +1,46 @@
+// Erstelle eine Zufallszahl zwischen 5 und 10
+
+let randomnumber = Math.random() * 6 + 5;
+let random = Math.floor(randomnumber);
+
+// Wenn der Wert 10 ist gib aus Ten
+
+if(random == 10)
+{
+    console.log("Ten");
+}
+
+// Wenn der Wert 9 ist gib aus Nine
+
+if(random == 9)
+{
+    console.log("Nine");
+}
+
+// Wenn der Wert 8 ist gib aus Eight
+
+if(random == 8)
+{
+    console.log("Eight");
+}
+
+// Wenn der Wert 7 ist gib aus Seven
+
+if(random == 7)
+{
+    console.log("Seven");
+}
+
+// Wenn der Wert 6 ist gib aus Six
+
+if(random == 6)
+{
+    console.log("Six");
+}
+
+// Wenn der Wert 5 ist gib aus Five
+
+if(random == 5)
+{
+    console.log("Five");
+}
