ce1ec98f9b131df98fe29d4ef7c8d62fc49976d6
matze2006 <matthias.h06@gmail.com>
2025-01-07 08:26:12 +0100

    09.1 Aufgabe 1 done

---
+++ JavaScript/09.1 Aufgabe 1/script.js
@@ -0,0 +1,19 @@
+for(let i = 1; i <= 222; i++)
+{
+    if(i % 5 == 0 && i % 7 == 0)
+    {
+        console.log("FooBar");
+    }
+    else if(i % 7 == 0)
+    {
+        console.log("Bar");
+    }
+    else if(i % 5 == 0)
+    {
+        console.log("Foo");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
