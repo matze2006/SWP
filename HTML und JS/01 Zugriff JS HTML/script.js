4ad1e598f3c7b8528e4a244a67b091ba0e47be74
matze2006 <matthias.h06@gmail.com>
2025-01-24 15:39:31 +0100

    01 Zugriff JS HTML done

--- HTML und JS/01 Zugriff JS HTML/script.js
+++ HTML und JS/01 Zugriff JS HTML/script.js
@@ -1,4 +1,59 @@
-document.getElementById("button").addEventListener("click", function(){
-    const element = document.getElementById("content");
-    element.innerHTML = "Super";
+document.getElementById("Addieren").addEventListener("click", function(){
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+    
+    if(isNaN(value1) || isNaN(value2))
+    {
+        alert("Invalid value");
+    }
+    else
+    {
+        const result = parseFloat(value1) + parseFloat(value2);
+        document.getElementById("result").innerText = result;
+    }
+});
+
+document.getElementById("Subtrahieren").addEventListener("click", function(){
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+    
+    if(isNaN(value1) || isNaN(value2))
+    {
+        alert("Invalid value");
+    }
+    else
+    {
+        const result = parseFloat(value1) - parseFloat(value2);
+        document.getElementById("result").innerText = result;
+    }
+});
+
+document.getElementById("Multiplizieren").addEventListener("click", function(){
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+    
+    if(isNaN(value1) || isNaN(value2))
+    {
+        alert("Invalid value");
+    }
+    else
+    {
+        const result = parseFloat(value1) * parseFloat(value2);
+        document.getElementById("result").innerText = result;
+    }
+});
+
+document.getElementById("Dividieren").addEventListener("click", function(){
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+    
+    if(isNaN(value1) || isNaN(value2))
+    {
+        alert("Invalid value");
+    }
+    else
+    {
+        const result = parseFloat(value1) / parseFloat(value2);
+        document.getElementById("result").innerText = result;
+    }
 });
