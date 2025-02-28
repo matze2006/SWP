78f5f0948a1bb117422b94ceac4afc1a3cd57203
matze2006 <matthias.h06@gmail.com>
2025-02-28 16:40:23 +0100

    03 Football-API 70%

--- Javascript und Ajax/03 Football-API/script.js
+++ Javascript und Ajax/03 Football-API/script.js
@@ -1,25 +1,36 @@
-const loadData = ()=>{
-    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result)=>{
-        result.json().then((data)=>{
+//Tabelle
+const loadData = () => {
+    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
+        result.json().then((data) => {
             console.log(data);
             fillTable(data);
         })
     })
 }
 
-const fillTable = (data)=>{
+const fillTable = (data) => {
     let html = "";
     let position = 1;
 
+    html += "<div id='table'>"
+
     data.forEach(element => {
-        html += "<div class='flex m-10 border items-center'>";
-        html += "<div class=''>" + position + "." + "</div>";
+        html += "<div id='" + element.teamInfoId + "' class='flex border items-center mb-10'>";
+        html += "<div class='ml-10'>" + position + "." + "</div>";
         html += "<div class='w-20 h-20 ml-20'>" + "<img src='" + element.teamIconUrl + "'>" + "</div>";
         html += "<div class='ml-20'>" + element.teamName + "</div>";
         html += "</div>"
         position++;
     });
-    document.getElementById("output").innerHTML=html;
+    document.getElementById("output").innerHTML = html;
+    html += "</div>"
+    
+    //Team anzeigen
+    document.getElementById("table").addEventListener("click", (event) => {
+        alert("clicked");
+    });
 }
 
 loadData();
+
+
