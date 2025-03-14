3a8b49d9975c67b12ef6b14f9cff215db1f70662
matze2006 <matthias.h06@gmail.com>
2025-03-14 16:23:58 +0100

    2 Aufgabe Komponenten

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,20 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/1 Setup React & Tailwind & erste Komponente/Card.jsx'
-//import Liste from './components/2 Aufgabe Komponenten/Liste.jsx'
+import Liste from './components/2 Aufgabe Komponenten/Liste.jsx'
 import Button from './components/2 Aufgabe Komponenten/Button.jsx'
-//import Cards from './components/2 Aufgabe Komponenten/Cards.jsx'
+import Cards from './components/2 Aufgabe Komponenten/Cards.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/*1*/}
       <div className="flex">
         <Card name={"Lenz"} jobtitle={"AV"} description={"lässig"}/>
         <Card name={"Sams"} jobtitle={"AV"} description={"auch lässig"}/>
       </div>
-      <Button Text={"Sendar"}/>
+      {/*2*/}
+      <div className="mt-10">
+        <Liste Name={"Coffee"}/>
+        <Liste Name={"Tea"}/>
+        <Liste Name={"Beer"}/>
+      </div>
+      <div className="mt-10 mb-10">
+        <Button Text={"Sendar"}/>
+      </div>
+      <div className="mb-10">
+        <Cards Name={"John Doe"} Text={"Architect and Engineer"}/>
+      </div>
+      
     </>
   )
 }
---
+++ react/src/components/2 Aufgabe Komponenten/Liste.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Liste({Name}) {
+  return (
+    <div className="bg-cyan-300 hover:bg-cyan-600 text-white hover:text-black w-50 h-10 content-center pl-2">{Name}</div>
+  )
+}
