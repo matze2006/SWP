c74ceccacad6218b7284fe2341ae4570769dfbf7
matze2006 <matthias.h06@gmail.com>
2025-03-25 08:50:39 +0100

    4 Demo Projekt State und useEffect done

--- react/src/components/0_sites/PeopleContainer.jsx
+++ react/src/components/0_sites/PeopleContainer.jsx
@@ -3,6 +3,7 @@ import Card from './../3_Demo_Projekt_Cards_und_Daten/Card'
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
+  const [color, setColor] = useState("green")
 
   useEffect(()=>{
     fetch("https://67e1965b58cc6bf785266c57.mockapi.io/03/PeopleContainer/person").then(
@@ -10,13 +11,23 @@ export default function PeopleContainer() {
         setPeople(data);
       })
     );
-  }, {})
+  }, []);
+
+  useEffect(() =>{
+    alert("change");
+  },[color]);
 
   return (
-    <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-      {people.map(person => {
-        return <Card name={person.name} image={person.avatar} title={person.jobtitle}/>
-      })}
+    <div>
+      <button onClick={()=>{
+        let num = Math.random();        
+        setColor("orange" + num );
+      }}>Change Color</button>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+        {people.map(person => {
+          return <Card name={person.name} image={person.avatar} title={person.jobtitle}/>
+        })}
+      </div>
     </div>
   )
 }
