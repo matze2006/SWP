5f7e4374a5cf3be65551ca876bf3719fffe8f8d9
matze2006 <matthias.h06@gmail.com>
2025-03-25 09:27:43 +0100

    5 Demo Filter done

--- react/src/components/0_sites/PeopleContainer.jsx
+++ react/src/components/0_sites/PeopleContainer.jsx
@@ -3,28 +3,32 @@ import Card from './../3_Demo_Projekt_Cards_und_Daten/Card'
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
-  const [color, setColor] = useState("green")
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
   useEffect(()=>{
     fetch("https://67e1965b58cc6bf785266c57.mockapi.io/03/PeopleContainer/person").then(
       (res) => res.json().then(data =>{
         setPeople(data);
+        setFilteredPeople(data);
       })
     );
   }, []);
 
-  useEffect(() =>{
-    alert("change");
-  },[color]);
+  const filterPeople = (filter) =>{
+    let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+    setFilteredPeople(filtered)
+  }
 
   return (
-    <div>
-      <button onClick={()=>{
-        let num = Math.random();        
-        setColor("orange" + num );
-      }}>Change Color</button>
-      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-        {people.map(person => {
+    <div className='pl-4 pr-4'>
+      <div className='w-full fixed bg-white'>
+        <h2 className='font-bold text-3xl justify-self-center'>People</h2>
+        <input className="border m-5" type="text" placeholder="Search" onChange={(el) =>{
+          filterPeople(el.target.value);
+        }}/>
+      </div>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 pt-30'>
+        {filteredPeople.map(person => {
           return <Card name={person.name} image={person.avatar} title={person.jobtitle}/>
         })}
       </div>
